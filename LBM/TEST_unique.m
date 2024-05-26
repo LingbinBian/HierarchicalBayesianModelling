345d3242961a87347d4cdefb46c96223aad3b70a
LingbinBian <51219350+LingbinBian@users.noreply.github.com>
2024-05-26 20:37:27 +0800

    Delete LBM directory

--- LBM/TEST_unique.m
+++
@@ -1,7 +0,0 @@
-clear
-clc
-close all
-A = [1 0 0 1 0 0 ; 1 1 0 0 0 1; 1 0 0 1 0 0 ; 1 0 1 0 1 0 ; 1 0 0 1 0 0; 1 1 0 0 0 1];
-[Au,ia,ic] = unique(A, 'rows', 'stable')
-Counts = accumarray(ic, 1);
-Out = [Counts Au]
--- LBM/latent_initial.m
+++
@@ -1,8 +0,0 @@
-function [z] = latent_initial(N,K)
-% This function initializes the latent label vector by Uniform distribution.
-leng=0;
-while leng~=K
-z=randi([1,K],N,1);
-leng=length(unique(z));
-end
-
